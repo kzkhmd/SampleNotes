57e7b589cf2b5d168c7942eac850e19eefb02036
Kazuki Hamada <kazuki.ma9376@gmail.com>
2019-09-14 18:04:14 +0900

    Initial Commit

---
+++ SampleNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleNotes
+//
+//  Created by 濱田一輝 on 2019/09/14.
+//  Copyright © 2019 Kazuki Hamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
