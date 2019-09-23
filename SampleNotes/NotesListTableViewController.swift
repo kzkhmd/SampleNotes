b3b732598a3a012cda350c646aec7b95d8ec2416
Kazuki Hamada <kazuki.ma9376@gmail.com>
2019-09-23 16:11:09 +0900

    Add NotesListTableViewController.swift

---
+++ SampleNotes/NotesListTableViewController.swift
@@ -0,0 +1,132 @@
+//
+//  NotesListTableViewController.swift
+//  SampleNotes
+//
+//  Created by 濱田一輝 on 2019/09/14.
+//  Copyright © 2019 Kazuki Hamada. All rights reserved.
+//
+
+import UIKit
+
+class NotesListTableViewController: UITableViewController {
+    
+    let defaults = UserDefaults.standard
+    var NoteList:[(title:String, text:String)] = []
+
+    
+    @IBAction func addNewNote(_ sender: UIBarButtonItem) {
+        var NoteData:(title:String, text:String)
+        
+        NoteData.title = "新規メモ"
+        NoteData.text = ""
+        NoteList.append(NoteData)
+        
+        self.tableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.rowHeight = 50
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let LoadedNoteList = defaults.stringArray(forKey: "NoteList")
+        var NoteData:(title:String, text:String)
+        var num = 1
+        
+        // メモ一覧は毎回UserDefaultsから読み込むので、NoteListはここで必ず初期化する
+        NoteList = []
+        
+        if LoadedNoteList != nil {
+            for text in LoadedNoteList! {
+                NoteData.title = String(num)
+                NoteData.text = text
+                
+                NoteList.append(NoteData)
+                num += 1
+            }
+        }else{
+            print("LoadedNoteList is nil!")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return NoteList.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let NoteData = NoteList[(indexPath as NSIndexPath).row]
+        
+        cell.textLabel?.text = NoteData.title
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowNotes" {
+            let NextView = segue.destination as! ViewController
+            
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                NextView.NoteList = NoteList
+                NextView.row = (indexPath as NSIndexPath).row
+            }
+        }
+    }
+    
+}
--- SampleNotes/ViewController.swift
+++ SampleNotes/ViewController.swift
@@ -8,11 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet weak var TextView: UITextView!
+    
+    let defaults = UserDefaults.standard
+    
+    var NoteList:[(title:String, text:String)] = []
+    var TextList:[String] = []
+    var row:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        TextView.text = NoteList[row].text
+        
+        for NoteData in NoteList {
+            TextList.append(NoteData.text)
+        }
+    }
+    
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        // TextViewが編集が開始されたらNavigationBarの右上にか「完了」ボタンを表示
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "完了", style: .done, target: self, action: #selector(endEditing))
+        
+        return true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        // TextViewの編集が終了したらNavigationBarの右上の「完了」ボタンを消す
+        self.navigationItem.rightBarButtonItem = nil
+        
+        TextList[row] = TextView.text
+        
+        print(TextList)
+        
+        defaults.set(TextList, forKey: "NoteList")
+    }
+    
+    @objc func endEditing() {
+        self.view.endEditing(true)
     }
 
 
